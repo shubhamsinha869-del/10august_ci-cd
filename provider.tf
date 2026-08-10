2e4ec154b642d1d217c90cf56ef84181419b9b72
shubham10may <shubhi086@gmail.com>
2026-08-10 20:26:52 +0530

    Add Terraform CI pipeline-5

--- provider.tf
+++ provider.tf
@@ -1,12 +1,59 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.0.0" # Specify the version
-    }
-  }
-}
+trigger:
+  branches:
+    include:
+      - feature/resource-group
 
-provider "azurerm" {
-  features {} # Required block for AzureRM provider
-}
+pool:
+  vmImage: ubuntu-latest
+
+stages:
+
+- stage: CI
+  displayName: Terraform CI
+
+  jobs:
+  - job: TerraformCI
+    displayName: Terraform Validation and Plan
+
+    steps:
+
+    - script: |
+        TERRAFORM_VERSION="1.15.5"
+
+        echo "Installing Terraform ${TERRAFORM_VERSION}"
+
+        curl -fsSL \
+          https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip \
+          -o terraform.zip
+
+        unzip -o terraform.zip
+
+        sudo mv terraform /usr/local/bin/terraform
+
+        terraform version
+      displayName: Install Terraform
+
+    - script: |
+        terraform init
+      displayName: Terraform Init
+
+    - script: |
+        terraform fmt -check -recursive
+      displayName: Terraform Format Check
+
+    - script: |
+        terraform validate
+      displayName: Terraform Validate
+
+    - task: AzureCLI@2
+      displayName: Terraform Plan
+      inputs:
+        azureSubscription: 'terraform-azure-connection'
+        scriptType: bash
+        scriptLocation: inlineScript
+        inlineScript: |
+          echo "Checking Azure login..."
+          az account show
+
+          echo "Running Terraform Plan..."
+          terraform plan
